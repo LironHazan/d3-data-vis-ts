bbf1afceb9ad66626336c830b6fa53db0c217d70
lironh <lironenk@gmail.com>
2020-10-30 08:59:03 +0200

    feat(b): ordering

--- src/isolate_exp/disjointed/disjointed.ts
+++ src/isolate_exp/disjointed/disjointed.ts
@@ -1,5 +1,6 @@
 import * as d3 from 'd3';
 import {ExpModel, FirstLevelNode, Link, Node, NodeType, SecondLevelNode} from './exp-model';
+import {transformToGraphModel} from './utils';
 
 function graph(data: ExpModel<any, any>, width = 680, height = 680, ) {
 
@@ -87,40 +88,9 @@ function graph(data: ExpModel<any, any>, width = 680, height = 680, ) {
     return svg.node();
 }
 
-function transformToGraphModel<N  extends { group: string, id: string }, L extends { target: any, source: any}>(data: ExpModel<N, L>): ExpModel<any, Link> {
-    const nodes = data.nodes
-        .map( (node: any) => {
-        node.size = 2;
-        node.shape = 'circle';
-        node.type = node.group === 'Cited Works' ? NodeType.first_level : NodeType.second_level;
-       switch (node.type) {
-           case NodeType.first_level:
-               node.levelIntersection = false;
-               node. intersectionColor = '';
-               node.innerCircleColor = '';
-               node.innerCircleSize = 5;
-               node.innerCircleStroke = '';
-               node.outerCircleColor = '';
-               node.outerCircleSize = 5;
-               break;
-           case NodeType.second_level:
-                node.circleColor =  '';
-                node.circleStroke = '';
-       }
-        return node;
-    });
-
-    const links: Link[] = data.links.map( (link) => ({
-        source: link.source,
-        target: link.target
-    }));
-    return { nodes, links };
-}
-
 export function loadGraph() {
     d3.json('../data/graph.json').then(data => {
-        const tdata = transformToGraphModel(data);
-        graph(tdata);
+        graph(transformToGraphModel(data));
     });
 }
 
